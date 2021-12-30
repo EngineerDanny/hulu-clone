c320364639189eb77f75fb59601249d33bc9c523
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-12-30 20:13:51 +0000

    handle onClick for Nav items

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,34 @@
-import categories from "../utils/request";
+import { CATEGORIES } from "../utils/constants";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-function Nav() {
+const Nav = () => {
+  const router = useRouter();
   return (
-    <div className=" flex flex-col items-center" >
+    <div className=" flex flex-col items-center">
       <div className="-my-3" />
       <Image src="/hulu.png" className="" alt="" width="120" height="100" />
-
-      <div className="flex  flex-nowrap overflow-x-scroll max-w-sm  ">
-        {categories.map(({ title, url }) => {
-          return <span className=" flex-1" >{title}</span>;
-        })}
+      <div className="relative ">
+        <div className="  flex flex-nowrap overflow-x-scroll w-screen  scroll-smooth no-scrollbar">
+          {CATEGORIES.map(({ title, genre }) => {
+            return (
+              <button
+                className="mr-6 first:ml-5 "
+                onClick={() => {
+                  router.push(`?genre=${genre}`);
+                }}
+              >
+                <span className="whitespace-nowrap transition hover:text-white cursor-pointer active:text-pink-400">
+                  {title}
+                </span>
+              </button>
+            );
+          })}
+        </div>
+        <div className=" absolute top-0 right-0 h-10 w-10 bg-gradient-to-l from-gray-800" />
       </div>
     </div>
   );
-}
+};
 
 export default Nav;
