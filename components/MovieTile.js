358b098db2a986915a800693d3e025beef67e499
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-12-30 20:13:18 +0000

    minor MovieTile UI update

---
+++ components/MovieTile.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { BASE_IMG_ENDPOINT } from "../utils/constants";
+
+const MovieTile = ({ movie }) => {
+  return (
+    <div className="flex flex-col space-y-1 mt-7 items-center w-[80vw]">
+      <div className="h-64  w-full relative">
+        <Image
+          src={`${BASE_IMG_ENDPOINT}${
+            movie.poster_path || movie.backdrop_path
+          }`}
+          alt=""
+          objectFit="responsive"
+          layout="fill"
+          className=" rounded-lg"
+        />
+      </div>
+      <p className="truncate w-full  tracking-wider">{movie.overview}</p>
+      <h1 className="truncate w-full text-white font-bold">{movie.title}</h1>
+    </div>
+  );
+};
+
+export default MovieTile;
