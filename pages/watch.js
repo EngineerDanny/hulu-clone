b4d5d3013e6fdea4d2bb4a3a306b5e624f4c966d
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-01-12 12:20:36 +0000

    create new React Player

--- pages/watch.js
+++ pages/watch.js
@@ -1,3 +1,24 @@
+import ReactPlayer from "react-player/lazy";
+import screenfull from "screenfull";
+
 export default function Watch() {
-  return <div>Hello</div>;
+  return (
+    <div className=" bg-black  w-full !h-full">
+      <ReactPlayer
+        className="absolute top-0 left-0 w-full !h-full"
+        url={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+        width="100%"
+        height="100%"
+        controls={true}
+        playing={true}
+        onStart={() => {
+          screenfull.request();
+        }}
+        onEnded={() => {
+          screenfull.exit();
+        }}
+        wrapper={<div className="relative bg-black  w-full h-full" />}
+      />
+    </div>
+  );
 }
