041a37268c638da472acf0c1626d9cae7fcc454d
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-02-27 16:32:44 +0000

    refactor

--- components/Modal.js
+++ components/Modal.js
@@ -61,13 +61,6 @@ export default function Modal({ isOpen, onClose, movie }) {
             <div className="inline-block align-bottom bg-black rounded-lg text-left overflow-hidden shadow-xl transform transition-all  sm:align-middle sm:max-w-lg ">
               <div className="bg-black ">
                 <div className="sm:flex sm:items-start">
-                  {/* <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <XIcon
-                      className="h-6 w-6 text-red-600"
-                      aria-hidden="true"
-                      onClick={onClose}
-                    />
-                  </div> */}
                   <div className=" text-center ">
                     <Player url={trailerUrl} />
 
